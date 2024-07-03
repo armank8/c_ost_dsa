a0c21455be991740898f9d475af3ce650e9a18f5
Arman-Kabir <armankabir8@gmail.com>
2024-07-03 12:27:29 +0600

    task1

---
+++ 4th module/assign1.js
@@ -0,0 +1,20 @@
+
+function targetFinder(nums, target) {
+    let n = nums.length;
+    console.log(n);
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+
+    return -1
+}
+
+
+console.log(targetFinder([2, 6, 11, 7], 9));
+console.log(targetFinder([3, 2, 4], 6));
+console.log(targetFinder([3, 3], 6));
