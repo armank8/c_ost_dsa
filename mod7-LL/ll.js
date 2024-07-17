886146bca22374f14d421bc7530a3828958ea35f
Arman-Kabir <armankabir8@gmail.com>
2024-07-17 13:48:19 +0600

    ll insert and count done

--- mod7-LL/ll.js
+++ mod7-LL/ll.js
@@ -10,7 +10,7 @@ class LinkedList {
         this.head = null
     }
 
-    insert(value) {
+    insertAtEnd(value) {
         const newNode = new Node(value);
         console.log(newNode);
         if (this.head === null) {
@@ -23,12 +23,38 @@ class LinkedList {
             currNode.next = newNode;
         }
     }
+
+    insertAtFirst(value) {
+        const newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    }
     display() {
         let currNode = this.head;
+        let values=[];
         while (currNode !== null) {
-            console.log(currNode.value);
+            values.push(currNode.value);
+            // console.log(currNode.value);
             currNode = currNode.next;
         }
+        // console.log(this.count());
+        return values;
+    }
+
+    count() {
+        let currNode = this.head;
+        let count = 0;
+        // console.log(currNode);
+        while (currNode !== null) {
+            count++;
+            // console.log(currNode.value, count);
+            currNode = currNode.next;
+        }
+        return count;
     }
 
 }
@@ -41,11 +67,13 @@ class LinkedList {
 
 
 const ll = new LinkedList();
-ll.insert(5);
-ll.insert(10);
-ll.insert(11);
-ll.insert(12);
-ll.display();
+ll.insertAtEnd(5);
+ll.insertAtEnd(10);
+ll.insertAtEnd(11);
+ll.insertAtFirst(12);
+ll.insertAtFirst(13);
+console.log(ll.display());
+console.log(ll.count());
 // const ll1 = new LinkedList(10);
 
 console.log(ll);
