4df9d9d8485e33e0b44136053b7570fc6c43b00e
Arman-Kabir <armankabir8@gmail.com>
2024-08-13 16:25:44 +0600

    ll created

--- mod7-LL/cm/partitionList-10.js
+++ mod7-LL/cm/partitionList-10.js
@@ -1,3 +1,8 @@
+function ListNode(val) {
+    this.val = val;
+    this.next = null
+}
+
 function main(input) {
     console.log(input);
     const inputs = input.split('\n');
@@ -7,8 +12,20 @@ function main(input) {
     const X = parseInt(inputs[2]);
     console.log(N, values, X);
 
-    
+    // creating LL
+    let head = new ListNode(values[0]);
+    let current = head;
+    for (let i = 1; i < N; i++) {
+        current.next = new ListNode(values[i]);
+        current = current.next;
+    }
+
+    return head;
+
 }
 
 let input = "5\n5 4 3 2 1\n3";
-main(input);
+console.log(main(input));
+
+console.log(new ListNode(5))
+console.log(new ListNode(4));
