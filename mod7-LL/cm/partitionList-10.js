6ec8cee98118c848fd8a7ff319af689a5d31de0d
Arman-Kabir <armankabir8@gmail.com>
2024-08-13 09:32:33 +0600

    inputs parsed

--- mod7-LL/cm/partitionList-10.js
+++ mod7-LL/cm/partitionList-10.js
@@ -1,5 +1,14 @@
-function partitionList(){
+function main(input) {
+    console.log(input);
+    const inputs = input.split('\n');
+    console.log(inputs);
+    const N = parseInt(inputs[0]);
+    const values = inputs[1].split(' ').map(Number);
+    const X = parseInt(inputs[2]);
+    console.log(N, values, X);
 
+    
 }
 
-console.log(partitionList());
+let input = "5\n5 4 3 2 1\n3";
+main(input);
