006e88a8f5ec509d0f4dc8e663fb6d92e886b855
Arman-Kabir <armankabir8@gmail.com>
2024-08-14 09:27:52 +0600

    ll partition done

--- mod7-LL/cm/partitionList-10.js
+++ mod7-LL/cm/partitionList-10.js
@@ -20,8 +20,45 @@ function main(input) {
         current = current.next;
     }
 
-    return head;
+    let lessHead = new ListNode(0);
+    let greaterHead = new ListNode(0);
+    console.log(lessHead, greaterHead);
 
+    let less = lessHead;
+    let greater = greaterHead;
+    current = head;
+    console.log(less, greater);
+
+    while (current !== null) {
+        if (current.val < X) {
+            less.next = current;
+            less = less.next;
+        } else {
+            greater.next = current;
+            greater = greater.next;
+        }
+        current = current.next;
+    }
+
+    console.log(lessHead);
+
+    greater.next = null;
+
+    less.next = greaterHead.next;
+
+    console.log(lessHead);
+
+    let result = [];
+    current = lessHead.next;
+
+    while (current !== null) {
+        result.push(current.val);
+        current = current.next;
+
+    }
+
+    console.log(result.join(' -> '));
+    return head;
 }
 
 let input = "5\n5 4 3 2 1\n3";
