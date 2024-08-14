cb37288c5a1c451f08efee859f8f38cf783b9f65
Arman-Kabir <armankabir8@gmail.com>
2024-08-14 16:10:07 +0600

    add binary started

--- mod-5-string/subeen/addBinary.js
+++ mod-5-string/subeen/addBinary.js
@@ -1,6 +1,17 @@
-function main() {
+function main(a, b) {
+    let lenA = a.length;
+    let lenB = b.length;
+    let lenC = 3;
 
+    console.log(lenA, lenB);
+    if (lenA < lenB) {
+        a = a.padStart(lenB,0);
+        console.log(a);
+    } else {
+        b = b.padStart(lenA,0);
+        console.log(b);
+    }
 }
 
 
-console.log(main());
+console.log(main("11", "1"));
