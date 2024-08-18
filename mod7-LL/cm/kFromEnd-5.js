7edbc180f81990250531c9577507a1bb96b9717a
Arman-Kabir <armankabir8@gmail.com>
2024-08-18 17:42:03 +0600

    kth elem half done

--- mod7-LL/cm/kFromEnd-5.js
+++ mod7-LL/cm/kFromEnd-5.js
@@ -17,17 +17,45 @@ function llCreation(N, values) {
     return head;
 }
 
+function deleteKth(N, K, head) {
+    console.log(N, K, head);
+
+    let slow = head;
+    let fast = head;
+    let prev = null;
+    // let curr
+
+    for (let i = 1; i < K; i++) {
+        fast = fast.next;
+    }
+    console.log(fast);
+
+    while (fast.next !== null) {
+        fast = fast.next;
+        prev=slow;
+        slow = slow.next;
+    }
+    console.log(fast, slow);
+    console.log(prev);
+
+    prev.next = fast;
+
+    console.log(head);
+
+}
 
 function main(input) {
     let N = parseInt(input.split('\n')[0].split(' ')[0]);
     let K = parseInt(input.split('\n')[0].split(' ')[1]);
     let values = input.split('\n')[1].split(' ').map(Number);
-    console.log(N,K, values);
+    console.log(N, K, values);
 
     let ll = llCreation(N, values);
+    console.log(ll);
+    let kth = deleteKth(N, K, ll);
 }
 
 
 
-let input = "5 2\n1 2 3 4 5";
+let input = "5 5\n1 2 3 4 5";
 console.log(main(input));
