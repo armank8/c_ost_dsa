b5a87e1a39ae69559a7aa658fc83d8859da5bf76
Arman-Kabir <armankabir8@gmail.com>
2024-08-26 19:43:23 +0600

    next greater elem

--- mod8-stack-queue/cm/nextGreaterElements-9.js
+++ mod8-stack-queue/cm/nextGreaterElements-9.js
@@ -1,4 +1,23 @@
-function nextGreaterElement() {
+function nextGreaterElement(N, heights) {
+    console.log(N);
+    console.log(heights);
+    let results = [];
+    let greater = 0;
+
+    for (let i = 0; i < heights.length - 1; i++) {
+        // console.log(i);
+        for (let j = i + 1; j < heights.length; j++) {
+            // console.log(j);
+            if (heights[j] > heights[i]) {
+                console.log(heights[i],heights[j])
+                // heights[i] = heights[j];
+                results.push(heights[j]);
+                break
+            }
+
+        }
+    }
+    console.log(results);
 
 }
 function main(input) {
@@ -26,11 +45,12 @@ function main(input) {
         console.log(N);
         console.log(values);
 
-        console.log(nextGreaterElement(values));
+        console.log(nextGreaterElement(N, values));
     }
 
 
 }
 
-let input = "2\n7\n2 7 3 5 4 6 8\n5\n1 2 3 4 5";
+// let input = "2\n7\n2 7 3 5 4 6 8\n5\n1 2 3 4 5";
+let input = "1\n7\n2 7 3 5 4 6 8";
 console.log(main(input));
