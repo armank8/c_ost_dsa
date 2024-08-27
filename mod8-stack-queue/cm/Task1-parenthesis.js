dd33fe28be5925fe40372d83fda03292eaf266b0
Arman-Kabir <armankabir8@gmail.com>
2024-08-27 10:48:59 +0600

    parenthesis

---
+++ mod8-stack-queue/cm/Task1-parenthesis.js
@@ -0,0 +1,44 @@
+function parenthesisChecker(s) {
+    console.log(s);
+    bracketMap = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    }
+
+    let stk = [];
+
+    for (let i = 0; i < s.length; i++) {
+        console.log(i);
+        console.log(s[i]);
+
+        if (bracketMap[s[i]]) {
+            stk.push(s[i]);
+        } else {
+            let stkVal = stk.pop();
+            if (bracketMap[stkVal] === s[i]) {
+                continue
+            } else {
+                return false;
+            }
+        }
+    }
+
+    console.log(stk);
+
+    return stk.length ===0;
+}
+
+
+function main(s) {
+    console.log(s);
+    console.log(parenthesisChecker(s));
+}
+// Time Complexity- O(n) && Space Complexity- O(n)
+// ---------------------------------------------
+// let input1 = "(){}[]"
+// let input = "()"
+// let input = "(]"
+// let input4 = "{()}"
+let input = "{(})"
+console.log(main(input));
