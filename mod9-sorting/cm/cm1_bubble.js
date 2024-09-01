27e41b61cbbc426a90a55349e3d149de079a19e6
Arman Kabir <armankabir8@gmail.com>
2024-09-01 10:20:43 +0600

    bubbleSort

--- mod9-sorting/cm/cm1_bubble.js
+++ mod9-sorting/cm/cm1_bubble.js
@@ -1,21 +1,31 @@
-
-function main(arr) {
-    console.log(arr);
+function task(N, arr) {
+    console.log(N, arr);
 
     let temp;
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
-            console.log(i, j);
-            if (arr[j] > arr[j+1]) {
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
             }
         }
     }
-
-    return arr;
+    return arr.join(' ');
 }
 
-let arr = [7, 8, 3, 1, 2];
-console.log(main(arr));
+function main(input) {
+    // managing input values
+    console.log(input);
+    let inputs = input.split('\n');
+    let N = parseInt(inputs[0]);
+    let arr = inputs[1].split(' ').map(Number);
+
+    console.log(N, arr);
+
+    // task functions
+    console.log(task(N, arr));
+}
+
+let input = "5\n10 8 14 3 10";
+console.log(main(input));
