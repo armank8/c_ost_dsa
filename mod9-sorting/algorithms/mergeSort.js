d15a7164ccec8324ca13992ce21547bca6236aba
Arman Kabir <armankabir8@gmail.com>
2024-09-04 18:55:04 +0600

    merged sort

--- mod9-sorting/algorithms/mergeSort.js
+++ mod9-sorting/algorithms/mergeSort.js
@@ -1,6 +1,29 @@
-function conquer(arr,si,mid,ei){
-    let merged=[];
-    
+function conquer(arr, si, mid, ei) {
+    let merged = [];
+    let idx1 = si;
+    let idx2 = mid + 1;
+    let x = 0;
+
+    while (idx1 <= mid && idx2 <= ei) {
+        if (arr[idx1] <= arr[idx2]) {
+            merged[x++] = arr[idx1++];
+        } else {
+            merged[x++] = arr[idx2++];
+        }
+    }
+
+    while (idx1 <= mid) {
+        merged[x++] = arr[idx1++];
+    }
+    while (idx2 <= ei) {
+        merged[x++] = arr[idx2++];
+    }
+
+    for (let i = 0 ,let j = si; i < merged.length; i++, j++) {
+        arr[j] = merged[i];
+    }
+
+    console.log(arr);
 }
 
 function divide(arr, si, ei) {
@@ -9,7 +32,7 @@ function divide(arr, si, ei) {
     console.log(si, ei, mid);
     divide(arr, si, mid);
     divide(arr, mid + 1, ei);
-    conquer(arr,si,mid,ei);
+    conquer(arr, si, mid, ei);
 }
 function task(N, arr) {
     console.log(N, arr);
