9dc6d555c287bdee7f6bd3c2b70aec13e65ed3ca
Arman Kabir <armankabir8@gmail.com>
2024-09-06 17:45:55 +0600

    perfect square binary search done

--- mod10-searching/cm/cm-10perfect_square.js
+++ mod10-searching/cm/cm-10perfect_square.js
@@ -18,8 +18,22 @@ function binarySearchR(N, low, high) {
 }
 
 // binarySearch Iterative
-function binarySearchI() {
+function binarySearchI(N, low, high) {
+    console.log(N, low, high);
 
+    while (low <= high) {
+        let mid = parseInt((low + high) / 2);
+        let sq = mid * mid;
+        console.log(sq,mid);
+        if (sq == N) {
+            return 'YES';
+        } else if (sq < N) {
+            low = mid + 1;
+        } else {
+            high = mid - 1
+        }
+    }
+    return 'NO';
 }
 // brute force
 // function task(N, arr) {
@@ -43,12 +57,13 @@ function main(input) {
     let high = parseInt(N / 2);
 
     // binarySearch recursion
-    console.log(binarySearchR(N, low, high));
+    // console.log(binarySearchR(N, low, high));
 
     // binarySearch Iterative
-    console.log(binarySearchI());
+    console.log(binarySearchI(N, low, high));
 }
 
-let input = "49";
-// let input = "24";
+// let input = "49";
+// let input = "25";
+let input = "24";
 console.log(main(input));
