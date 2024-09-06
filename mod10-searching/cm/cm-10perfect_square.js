939b2472a81d68f8c5aaca29eae90acf73814b26
Arman Kabir <armankabir8@gmail.com>
2024-09-06 12:45:05 +0600

    perfect square recursion with problem

--- mod10-searching/cm/cm-10perfect_square.js
+++ mod10-searching/cm/cm-10perfect_square.js
@@ -1,34 +1,43 @@
-function binarySearch(N) {
-    console.log(N);
-
-    let low = 1;
-    let high = parseInt(N / 2);
+function binarySearch(N, low, high) {
     let mid = parseInt((low + high) / 2);
-    let sq = mid*mid;
+    let sq = mid * mid;
 
-    if(sq===N){
-        
+    console.log(N,low, high, mid, sq);
+
+    if (sq == N) {
+        return true;
+    } else if (sq > N) {
+        binarySearch(N, low, mid-1);
+    } else {
+        binarySearch(N, mid+1, high);
     }
 
-    console.log(N,low, high, mid,sq)
+    console.log(N,low, high, mid, sq);
+    // return false;
 }
 
 // brute force
-function task(N, arr) {
-    console.log(N);
-    for (let i = 1; i < N / 2; i++) {
-        if (i * i == N) {
-            return 'YES';
-        }
-    }
-    return 'NO';
-}
+// function task(N, arr) {
+//     console.log(N);
+//     for (let i = 1; i < N / 2; i++) {
+//         if (i * i == N) {
+//             return 'YES';
+//         }
+//     }
+//     return 'NO';
+// }
 
 function main(input) {
     // managing input values
     let N = parseInt(input);
     // console.log(task(N));
-    console.log(binarySearch(N));
+
+
+    let low = 1;
+    let high = parseInt(N / 2);
+
+    // binarySearch
+    console.log(binarySearch(N, low, high));
 }
 
 let input = "49";
