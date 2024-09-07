f2ee44fe596118b768bdca9557e56aa21f0a3809
Arman Kabir <armankabir8@gmail.com>
2024-09-07 13:35:54 +0600

    first greater element brute force

---
+++ mod10-searching/cm/cm_6_first_greater_element.js
@@ -0,0 +1,41 @@
+function task(n, k, arr, q1) {
+    console.log(n, k, arr, q1);
+    let i;
+    for (i = 0; i < n; i++) {
+        if (arr[i] > q1) {
+            console.log(arr[i]);
+            break;
+        }
+    }
+
+
+    return arr[i];
+}
+
+function main(input) {
+    // managing input values
+    console.log(input);
+    let inputs = input.split('\n');
+    console.log(inputs);
+    let n = parseInt(inputs[0].split(' ')[0]);
+    let k = parseInt(inputs[0].split(' ')[1]);
+    let arr = inputs[1].split(' ').map(Number);
+
+    let result = [];
+    for (let i = 2; i < k + 2; i++) {
+        console.log(parseInt(inputs[i]));
+
+        // task functions-- brute force
+        // result.push(task(n, k, arr, q1 = parseInt(inputs[i])));
+
+        // binary search
+
+        console.log(result);
+    }
+
+    // print result
+    console.log(result.join('\n'));
+}
+
+let input = "5 2\n3 8 10 10 14\n10\n7";
+console.log(main(input));
