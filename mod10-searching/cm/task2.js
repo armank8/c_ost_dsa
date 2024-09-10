a52f7ef234714e68faf82c2c7de18193f27ba8f7
Arman Kabir <armankabir8@gmail.com>
2024-09-10 07:51:10 +0600

    task2 not done

--- mod10-searching/cm/task2.js
+++ mod10-searching/cm/task2.js
@@ -1,18 +1,18 @@
-function task(n, arr) {
-    console.log(n, arr);
+function task(n, arr,x) {
+    console.log(n, arr,x);
 
     let low = 0;
     let high = n - 1;
     let mid;
-    let min = 0;
+    // let min = 0;
 
     while (low <= high) {
         mid = Math.floor((low + high) / 2);
         console.log(mid);
-        if (arr[mid]==min){
+        if (arr[mid] == x) {
             return arr[mid];
-        }else if(arr[mid]>min){
-            high=mid-1;
+        } else if (arr[mid] > x) {
+            high = mid - 1;
         }
     }
 
@@ -25,11 +25,14 @@ function main(arr) {
     // managing input values
     console.log(arr);
     n = arr.length;
+    let x=3;
 
     // task functions
-    console.log(task(n, arr));
+    console.log(task(n, arr,x));
 }
 
-let nums = [ 0, 1, 2,4, 5, 6, 7];
+let nums = [4, 6, 9, 10, 12, 19, 20, 1, 2, 3];
+
+// let nums = [ 0, 1, 2,4, 5, 6, 7];
 // let nums = [4, 5, 6, 7, 0, 1, 2];
 console.log(main(nums));
