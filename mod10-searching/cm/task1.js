08dec1a34daeae80bd7cec0672d4369e9187af2f
Arman Kabir <armankabir8@gmail.com>
2024-09-11 16:24:23 +0600

    task1-2 done

--- mod10-searching/cm/task1.js
+++ mod10-searching/cm/task1.js
@@ -1,16 +1,13 @@
 function task(nums, target) {
-    // console.log(nums, target);
-
     let low = 0, high = nums.length - 1;
-    console.log(low, high);
-    let mid;
     let pos = -1;
 
-    while (low < high) {
-        mid = Math.floor((low + high) / 2);
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
 
         if (nums[mid] === target) {
             pos = mid;
+            console.log(pos);
             return pos;
         } else if (nums[mid] < target) {
             low = mid + 1
@@ -21,13 +18,6 @@ function task(nums, target) {
     return pos;
 }
 
-function main(nums, target) {
-    // console.log(nums, target);
-
-    // task functions
-    console.log(task(nums, target));
-}
-
-let nums = [-1, 0, 3, 5, 9, 12], target = 9;
+let nums = [-1, 0, 3, 5, 9, 12], target =9;
 // let nums = [-1, 0, 3, 5, 9, 12], target = 2;
-console.log(main(nums, target));
+console.log(task(nums, target));
--- mod10-searching/cm/task2.js
+++ mod10-searching/cm/task2.js
@@ -1,16 +1,9 @@
-function task(n, arr, x) {
-    console.log(n, arr, x);
-
-    let low = 0;
-    let high = n - 1;
-    let mid;
-    let min = Infinity;
-    console.log(min);
-    // let min = 0;
+function task(arr) {
+    let low = 0, high = arr.length - 1, min = Infinity;
 
     while (low < high) {
-        mid = Math.floor((low + high) / 2);
-        console.log(mid);
+        let mid = Math.floor((low + high) / 2);
+
         if (arr[mid] > arr[high]) {
             min = Math.min(arr[high], min);
             low = mid + 1;
@@ -20,22 +13,15 @@ function task(n, arr, x) {
             high = mid;
         }
     }
+    console.log(min);
     return arr[low];
     // return min;
 }
 
-function main(arr) {
-    // managing input values
-    console.log(arr);
-    n = arr.length;
-    let x = 3;
+// let nums = [4, 6, 9, 10, 12, 19, 20, 1, 2, 3];
 
-    // task functions
-    console.log(task(n, arr, x));
-}
-
-let nums = [4, 6, 9, 10, 12, 19, 20, 1, 2, 3];
-
-// let nums = [ 0, 1, 2,4, 5, 6, 7];
+let nums = [3, 4, 5, 1, 2];
 // let nums = [4, 5, 6, 7, 0, 1, 2];
-console.log(main(nums));
+// let nums = [ 0, 1, 2,4, 5, 6, 7];
+// let nums = [11,13,15,17] ;
+console.log(task(nums));
