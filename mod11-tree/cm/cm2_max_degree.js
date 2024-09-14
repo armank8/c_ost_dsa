2752e36e2006416a16312ad0b88f31c727f14315
Arman Kabir <armankabir8@gmail.com>
2024-09-14 19:30:00 +0600

    max_degree done

--- mod11-tree/cm/cm2_max_degree.js
+++ mod11-tree/cm/cm2_max_degree.js
@@ -1,4 +1,25 @@
+function task(N, nodes) {
+    console.log(N, nodes);
 
+    let adjacencyList = Array.from({ length: N + 1 }, () => [])
+
+    console.log(adjacencyList);
+
+    for (let i = 1; i < N; i++) {
+        console.log(i);
+        let [a, b] = nodes[i - 1];
+        console.log(a, b);
+        adjacencyList[a].push(b);
+        adjacencyList[b].push(a);
+    }
+    console.log(adjacencyList);
+    let max = 0;
+    for (let i = 0; i <= N; i++) {
+        max = Math.max(max,adjacencyList[i].length)
+    }
+    console.log(max);
+    return max;
+}
 
 function main(input) {
     // managing input values
