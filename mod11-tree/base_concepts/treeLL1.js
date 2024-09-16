191f8679a3beea9937a310745b20a8bd27d2e725
Arman Kabir <armankabir8@gmail.com>
2024-09-16 09:11:59 +0600

    dfs postorder and bfs

--- mod11-tree/base_concepts/treeLL1.js
+++ mod11-tree/base_concepts/treeLL1.js
@@ -37,17 +37,36 @@ class BinaryTree {
         }
     }
 
-    dfsPreOrder(node){
-        if(!node) return ;
+    dfsPreOrder(node) {
+        if (!node) return;
         console.log(node.value);
         this.dfsPreOrder(node.left);
         this.dfsPreOrder(node.right);
     }
-    dfsInOrder(node){
-        if(!node) return ;
+    dfsInOrder(node) {
+        if (!node) return;
         this.dfsInOrder(node.left);
         console.log(node.value);
         this.dfsInOrder(node.right);
+    }
+    dfsPostOrder(node) {
+        if (!node) return;
+        this.dfsPostOrder(node.left);
+        this.dfsPostOrder(node.right);
+        console.log(node.value);
+    }
+
+    bfs(node) {
+        if (!node) return;
+        let queue = [node];
+        while (queue.length > 0) {
+            let currNode = queue.shift();
+            console.log(currNode.value);
+
+            if(currNode.left) queue.push(currNode.left);
+            if(currNode.right) queue.push(currNode.right);
+
+        }
     }
 }
 
@@ -55,6 +74,8 @@ const tree = new BinaryTree();
 tree.insert([1, 2, 3, 4, 5, 6]);
 console.log(tree.dfsPreOrder(tree.root));
 console.log(tree.dfsInOrder(tree.root));
+console.log(tree.dfsPostOrder(tree.root));
+console.log(tree.bfs(tree.root));
 
 console.log(tree);
 
