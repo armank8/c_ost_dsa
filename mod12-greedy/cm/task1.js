9acf9b334c6d8b815098f2797b6ee9d864caa7eb
Arman Kabir <armankabir8@gmail.com>
2024-09-25 10:54:48 +0600

    task1 not done

--- mod12-greedy/cm/task1.js
+++ mod12-greedy/cm/task1.js
@@ -1,11 +1,22 @@
-function main(arr, truckSize) {
-    console.log(arr, truckSize);
+function main(boxTypes, truckSize) {
+    console.log(boxTypes, truckSize);
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    let totalUnits = 0;
+    console.log(boxTypes);
 
-    for (let i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
+
+    for (let i = 0; i < boxTypes.length; i++) {
+        for (let j = 0; j < boxTypes[i].length; j++) {
+            console.log(boxTypes[i][j]);
+
+
+            if (truckSize == 0) {
+                return numberOfUnits;
+            }
+        }
     }
 }
 
-let arr = [[1, 3], [2, 2], [3, 1]];
+let boxTypes = [[1, 3], [2, 2], [3, 1]];
 truckSize = 4;
-console.log(main(arr, truckSize));
+console.log(main(boxTypes, truckSize));
