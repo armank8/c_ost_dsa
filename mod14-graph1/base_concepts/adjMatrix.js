b3c4c2d947efcee6c869bf7d30c132661061e1c5
Arman Kabir <armankabir8@gmail.com>
2024-10-08 08:41:17 +0600

    adjmatrix row col

--- mod14-graph1/base_concepts/adjMatrix.js
+++ mod14-graph1/base_concepts/adjMatrix.js
@@ -1,22 +1,31 @@
-function createAdjMatrix(numVertices){
+function createAdjMatrix(numVertices) {
     // initialize a 2D array filled with 0s
-    const adjMatrix = Array(numVertices).fill(0).map(()=>Array(numVertices).fill(0));
+    const adjMatrix = Array(numVertices).fill(0).map(() => Array(numVertices).fill(0));
 
     console.log(adjMatrix);
     return adjMatrix;
 }
 
 // add an edge to the matrix
-function addEdgeMatrix(matrix,v,w){
-    matrix[v][w] =1;
-    matrix[w][v] =1;
+function addEdgeMatrix(matrix, v, w) {
+    matrix[v][w] = 1;
+    matrix[w][v] = 1;
     return matrix;
 }
 
-function printMatrix(matrix){
-    for(let row of matrix){
-        console.log(row.join(" "));
+// print Matrix
+function printMatrix(matrix) {
+    const numVertices = matrix.length;
+
+    for (let row = 0; row < numVertices; row++) {
+        console.log(`Row ${row}:`);
+
+        for (let col = 0; col < numVertices; col++) {
+            console.log(`matrix[${row}][${col}] = ${matrix[row][col]}`);
+        }
+        console.log('');
     }
+
 }
 
 const numVertices = 4;
