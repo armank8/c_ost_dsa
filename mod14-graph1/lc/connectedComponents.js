8f16d0fee02e32796e742081bda8ba7f5c872a32
Arman Kabir <armankabir8@gmail.com>
2024-10-08 14:09:21 +0600

    connected components except bfs

---
+++ mod14-graph1/lc/connectedComponents.js
@@ -0,0 +1,42 @@
+function createAdjMatrix(n, edges) {
+    console.log(n, edges);
+    const adjMatrix = Array(n).fill(0).map(() => Array(n).fill(0));
+
+    for (let [a, b] of edges) {
+        console.log([a, b]);
+        adjMatrix[a][b] = 1;
+        adjMatrix[b][a] = 1;
+    }
+    console.log(adjMatrix);
+    return adjMatrix;
+}
+
+function bfs(startNode,adjMatrix,visited){
+    
+}
+
+function countComponentsBFS(n, edges) {
+    console.log(n, edges);
+    const adjMatrix = createAdjMatrix(n, edges);
+
+    const visited = Array(n).fill(false);
+    console.log(visited);
+
+    let components = 0;
+    for (let i = 0; i < n; i++) {
+        if(!visited[i]){
+            bfs(i,adjMatrix,visited);
+            components++;
+        }
+    }
+
+    return components;
+}
+n = 5;
+edges = [
+    [0, 1],
+    [1, 2],
+    [3, 4]
+];
+console.log(countComponentsBFS(n, edges));
+
