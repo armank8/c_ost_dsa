55b477f300baf5f8081619ce92dd75b61dd07f45
Arman-Kabir <armankabir8@gmail.com>
2024-11-13 17:12:59 +0600

    Task1

--- mod18/Tasks/Task1.js
+++ mod18/Tasks/Task1.js
@@ -1,6 +1,36 @@
-function topKFrequent(nums,k){
+function topKFrequent(nums, k) {
+  let hash = {};
+  for (let num of nums) {
+    hash[num] = (hash[num] | 0) + 1;
+  }
+  console.log(hash);
 
+  
+  let store = Array(nums.length + 1)
+    .fill(null)
+    .map(() => []);
+  console.log(store);
+
+
+  for (let num in hash) {
+    console.log(num);
+    store[hash[num]].push(Number(num));
+  }
+  console.log(store);
+
+
+  let result = [];
+  for (let i = store.length - 1; i >= 0; i--) {
+    console.log(i);
+    if(store[i]){
+        result.push(...store[i]);
+    }
+  }
+  console.log(result);
+  return result.slice(0,k);
 }
-let nums = [1,1,1,2,2,3];
-let k=2;
-console.log(topKFrequent(nums,k));
+
+
+let nums = [1, 1, 1, 2, 2, 3];
+let k = 2;
+console.log(topKFrequent(nums, k));
