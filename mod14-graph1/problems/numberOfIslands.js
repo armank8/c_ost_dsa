6a2bba634ee008faf618c3c509df30f94a1fb212
Arman-Kabir <armankabir8@gmail.com>
2025-03-01 08:40:05 +0600

    numIslands

--- mod14-graph1/problems/numberOfIslands.js
+++ mod14-graph1/problems/numberOfIslands.js
@@ -8,20 +8,41 @@ function numIslands(grid){
     let islandCount =0;
     const directions=[[1,0], [-1,0], [0,1], [0,-1]];
 
+    console.log(rows,cols);
+
     function bfs(r,c){
         let queue = [[r,c]];
         grid[r][c] = '0';
 
+        while(queue.length>0){
+            let [row,col] =queue.shift();
+
+            for(let [dr,dc] of directions){
+                let newRow =row+dr;
+                let newCol =col+dc;
+
+                if(newRow>=0 && newRow<rows && newCol>=0 && newCol<cols && grid[newRow][newCol] ==='1'){
+                    queue.push([newRow,newCol]);
+                    grid[newRow,newCol] = '0';
+                }
+            }
+        }
+
     }
 
-    console.log(rows,cols);
+ 
 
     // Traverse the grid
     for(let r=0;r<rows;r++){
         for(let c=0;c<cols;c++){
             console.log(grid[r][c]);
+            if(grid[r][c]==='1'){
+                islandCount++;
+                bfs(r,c);
+            }
         }
     }
+    return islandCount;
 
 }
 
