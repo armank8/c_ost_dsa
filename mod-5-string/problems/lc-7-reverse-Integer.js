7bc97b96b3f7f94f30e1abe0669308e17a60a616
Arman-Kabir <armankabir8@gmail.com>
2025-03-06 21:49:48 +0600

    reversed integer

---
+++ mod-5-string/problems/lc-7-reverse-Integer.js
@@ -0,0 +1,22 @@
+var reverse = function(x) {
+    console.log(x);
+    let INT_MAX = 2**31-1;
+    let INT_MIN = -(2**31);
+    console.log(INT_MAX," ", INT_MIN);
+
+    let inputs=x.toString().split('').reverse().join('');
+    let reversed = parseInt(inputs);
+    console.log(reversed);
+
+    if(x<0){
+        reversed = -reversed;
+    }
+
+    if(reversed<INT_MIN || reversed > INT_MAX){
+        return 0;
+    }
+    return reversed;
+};
+console.log(reverse(123));
+console.log(reverse(-124));
+// console.log(reverse(120));
