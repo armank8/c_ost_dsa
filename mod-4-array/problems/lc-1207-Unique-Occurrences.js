34a906734ffc0408cd6314f600884dccfdfc1e49
Arman-Kabir <armankabir8@gmail.com>
2025-03-08 16:59:44 +0600

    uniqueOccurrences

--- mod-4-array/problems/lc-1207-Unique-Occurrences.js
+++ mod-4-array/problems/lc-1207-Unique-Occurrences.js
@@ -1,11 +1,17 @@
 var uniqueOccurrences = function(arr) {
-    let s= new Set();
-    console.log(s);
+    let s= {};
 
     for(let i=0;i<arr.length;i++){
         console.log(arr[i]);
-        s.add(arr[i]);
+        s[arr[i]] = (s[arr[i]] | 0) + 1;
     }
     console.log(s);
+    console.log(Object.keys(s).length);
+
+    let freqSet = new Set(Object.values(s));
+    console.log(freqSet);
+
+    return Object.keys(s).length === freqSet.size;
 };
 console.log(uniqueOccurrences([1,2,2,1,1,3]));
+console.log(uniqueOccurrences([1, 2, 2, 3, 3, 3]));
