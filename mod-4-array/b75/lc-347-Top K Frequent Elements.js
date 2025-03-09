aa0a9a144a64803f1ac8e56dd5371d91814294dd
Arman-Kabir <armankabir8@gmail.com>
2025-03-09 15:20:32 +0600

    top K frequent elements

--- mod-4-array/b75/lc-347-Top K Frequent Elements.js
+++ mod-4-array/b75/lc-347-Top K Frequent Elements.js
@@ -1,4 +1,18 @@
+// lc 347. Top K Frequent Elements
 var topKFrequent = function(nums, k) {
-    
+    let hash={};
+    for(let num of nums){
+        console.log(num);
+        hash[num] = (hash[num] | 0) + 1;
+    }
+    console.log(hash);
+
+    let topK = Object.keys(hash)
+    .sort((a,b)=>hash[b]-hash[a])
+    .slice(0,k)
+    .map(Number)
+
+    // console.log(topK);
+    return topK;
 };
-console.log(topKFrequent([1,1,1,2,2,3]),2);
+console.log(topKFrequent([1,1,1,2,2,3],2));
