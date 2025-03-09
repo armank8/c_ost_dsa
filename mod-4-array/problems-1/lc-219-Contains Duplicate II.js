6d2b84b5155d5e54ff41f59f1082982f4b319154
Arman-Kabir <armankabir8@gmail.com>
2025-03-09 12:38:33 +0600

    contains duplicate 2

--- mod-4-array/problems-1/lc-219-Contains Duplicate II.js
+++ mod-4-array/problems-1/lc-219-Contains Duplicate II.js
@@ -1,5 +1,18 @@
+// 219. Contains Duplicate II
 var containsNearbyDuplicate = function(nums, k) {
-    
+    let map= new Map();
+
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i]) && (i- map.get(nums[i]) <=k) ){
+            return true;
+        }
+
+        map.set(nums[i],i);
+    }
+
+    console.log(map);
+    console.log(map.get(2));
+    return false;
 };
 console.log(containsNearbyDuplicate([1,2,3,1],3))
 console.log(containsNearbyDuplicate([1,0,1,1],1))
