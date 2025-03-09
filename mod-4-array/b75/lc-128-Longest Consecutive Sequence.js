d011564b4d74b3734a6258ec3194c1e8fe236c27
Arman-Kabir <armankabir8@gmail.com>
2025-03-09 22:00:03 +0600

    name renamed

---
+++ mod-4-array/b75/lc-128-Longest Consecutive Sequence.js
@@ -0,0 +1,5 @@
+// 128. Longest Consecutive Sequence
+var longestConsecutive = function(nums) {
+    
+};
+console.log(longestConsecutive([100,4,200,1,3,2]));
--- mod-4-array/problems-0/lc-215-kth largest element.js
+++ mod-4-array/problems-0/lc-215-kth largest element.js
@@ -1,16 +1,20 @@
 // 215. Kth Largest Element in an Array
+
 // var findKthLargest = function(nums, k) {
 //     nums.sort((a,b)=>b-a);
 
 //     console.log(nums[k-1]);
 //     return nums[k-1];
 // };
+
 // console.log(findKthLargest([3,2,1,5,6,4],2));
 // TimeComplexity - sorting takes O(n log n), slow for large arrays
 
 
+
 // Approach 2 : Min Heap(O (n log k) )- Efficient for Large n
 // Using JS Priority Queue - Min Heap
+
 var findKthLargest = function(nums, k) {
    let minHeap = [];
 
