5dfc89397f7c0e04d2f129faf15e094440d269a2
Arman-Kabir <armankabir8@gmail.com>
2025-03-11 23:10:36 +0600

    bt level order

--- mod-4-array/problems-1/lc-387-first Unique-.js
+++ mod-4-array/problems-1/lc-387-first Unique-.js
@@ -5,6 +5,8 @@ function firstUniqueChar(s) {
         // console.log(elm);
         hash[elm] = (hash[elm] || 0) + 1;
     }
+    console.log(hash);
+    
     for (let i = 0; i < s.length; i++) {
         // console.log(s[i]);
         if (hash[s[i]] === 1) {
@@ -16,4 +18,4 @@ function firstUniqueChar(s) {
 }
 
 console.log(firstUniqueChar('leetcode'));
-console.log(firstUniqueChar('leetcode'));
+// console.log(firstUniqueChar('leetcode'));
--- mod11-tree/Problems 2/lc-102-Binary Tree Level Order Traversal.js
+++ mod11-tree/Problems 2/lc-102-Binary Tree Level Order Traversal.js
@@ -1,7 +1,40 @@
 // lc-102-Binary Tree Level Order Traversal.js
 
 var levelOrder = function(root) {
-    
+    if(!root) return [];
+    let result = [];
+    let queue = [root];
+
+    while(queue.length>0){
+        let levelSize = queue.length;
+        let levelNodes =[];
+
+        for(let i=0; i<levelSize; i++){
+            let node = queue.shift();
+            levelNodes.push(node.val)
+
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+        result.push(levelNodes);
+    }
+    return result;
 };
 
-console.log(levelOrder());
+
+// Example
+const tree = {
+    val: 3,
+    left: {
+        val: 9,
+        left: null,
+        right: null
+    },
+    right: {
+        val: 20,
+        left: { val: 15, left: null, right: null },
+        right: { val: 7, left: null, right: null }
+    }
+};
+
+console.log(levelOrder(tree));
