12bd837868ea211bef5b08e423d60045a1805318
Arman-Kabir <armankabir8@gmail.com>
2025-03-26 21:07:00 +0600

    console.log basics

---
+++ 30 days of js/Promise and Time/console.log-basics.js
@@ -0,0 +1,45 @@
+// console.log-basics.js
+
+// function test(...args) {
+//   console.log(args);
+// }
+// console.log(test(1, 2, 3));
+
+console.log(typeof console.log);
+/**
+ * Since console.log is a function, we can :
+ * Assign it to a variable
+ * Pass it as an argument
+ * Call it dynamically
+ */
+
+// Assign to a variable
+const log = console.log;
+log("This works");
+
+// Pass console.log as an argument
+// Since functions are first-class citizens in js,
+// console.log can be passed as an argument!. 
+function execute(fn){
+    fn("Logging via function parameter!");
+}
+execute(console.log);
+
+/** What type of Function is console.log?
+ 
+a. It is a method. Since console.log belongs to the
+console object , it is a method.Methods are functions 
+inside objects.
+
+console.log === window.console.log //true in browsers
+console.log === global.console.log  // true in node.js 
+ 
+b.It is a Variadic Function
+A viriadic function can take any number of arguments
+console.log can handle multiple arguments
+console.log("Hello", 42, {a:1}, [1, 2, 3] )
+This is different from normal functions,which often
+expect a fixed number of arguments
+
+
+ */
---
+++ 30 days of js/Promise and Time/lc-2627 Debounce.js
@@ -0,0 +1,42 @@
+// 2627: Debounce
+
+/**here debounce is a higher-order function(a function
+ *  that returns another function)--It takes two parameters
+ * fn: The function we want to debounce
+ * t: The delay time(in milliseconds)
+ * timer: is a variable that will store the timeout ID
+ * It is used to track and cancel the previous scheduled execution
+ *
+ *  ***** return function(...args) {} *****
+ * The function returns another function  that will
+ * actually handle debouncing
+ * (...args) means:
+ * It uses rest parameters (...) to accept any number of arguments
+ * These arguments (args) will be passed to fn when executed.
+ * So, in debounce , ...args lets us handle any function with
+ * any arguments
+ *  */
+
+
+
+// var debounce = function (fn, t) {
+//   let timer;
+//   return function (...args) {
+//     console.log("Debounce called with:", args);
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       console.log("Executing function...");
+//       fn(...args);
+//     }, t);
+//   };
+// };
+// let t = 50;
+// let calls = [
+//   { t: 50, inputs: [1] },
+//   { t: 75, inputs: [2] },
+// ];
+
+// const debouncedLog = debounce(console.log, 100);
+
+// debouncedLog("First Call");
+// debouncedLog("Second Call");
